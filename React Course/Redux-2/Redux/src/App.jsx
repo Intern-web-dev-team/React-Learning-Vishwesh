4926d72dd214183a0a3bab91500ded75e16a6785
VISHWESHERE <vissureddy412@gmail.com>
2025-03-25 12:48:44 +0530

    added redux

---
+++ React Course/Redux-2/Redux/src/App.jsx
@@ -0,0 +1,44 @@
+import {useSelector} from 'react-redux'
+import {useDispatch} from 'react-redux'
+import { increment, decrement, incrementByAmount, reset } from './counterSlice';
+import {useState} from 'react'
+
+function App() {
+  const [initValue, setInitValue] = useState(0);
+  const count = useSelector(State => State.counter.value)
+  const dispatch = useDispatch();
+
+  function handleClickInc(){
+    dispatch(increment());
+  }
+
+  function handleClickDec(){
+    dispatch(decrement());
+  }
+
+  function handleChange(e){
+    setInitValue(e.target.value);
+  }
+  function handleClickRes(){
+    dispatch(reset());
+  }
+
+  function handleButton(){
+    dispatch(incrementByAmount(initValue));
+  }
+
+  return (
+   <>
+   <h1>COUNT: {count}</h1>
+   
+   <button onClick={handleClickInc}>+</button>
+   <button onClick={handleClickDec}>-</button>
+   <input type="number" value={initValue} onChange={handleChange}/>
+   <button onClick={handleButton}>INC</button>
+   <button onClick={handleClickRes}>Reset</button>
+
+   </>
+  )
+}
+
+export default App
